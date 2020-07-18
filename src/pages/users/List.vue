30842d69b4758e31119b590fc92f25ada416dbec
Waldener Monteiro <jjrr019@gmail.com>
2020-07-18 15:55:24 -0300

    refactor(users): adicionando os ícones nos botões, finalizando os métodos crud

--- src/pages/users/List.vue
+++ src/pages/users/List.vue
@@ -2,23 +2,24 @@
   <section>
     <header>
       <nav>
-        <p class="text-right"><el-button size="medium" round type="primary" @click="handleRegister()">Register</el-button></p>
-      </nav>
-      <aside>
+        <p class="text-right"><el-button icon="fa fa-plus" size="medium" round type="primary" @click="handleRegister()"> Register</el-button></p>
         <p class="h1-text">Users</p>
-      </aside>
+      </nav>
     </header>
-    <el-table :data="users" :stripe="true" :border="true" style="width: 100%">
-      <el-table-column sortable prop="first_name" label="First Name" width="180"> </el-table-column>
-      <el-table-column sortable prop="last_name" label="Last Name" width="180"> </el-table-column>
-      <el-table-column sortable prop="email" label="E-mail"> </el-table-column>
-      <el-table-column align="right">
-        <template slot-scope="scope">
-          <el-button size="mini" round @click="handleEdit(scope.$index, scope.row)">Edit</el-button>
-          <el-button size="mini" round type="danger" @click="handleDelete(scope.$index, scope.row)">Delete</el-button>
-        </template>
-      </el-table-column>
-    </el-table>
+    <article>
+      <el-table empty-text="No data" :data="users" :stripe="true" :border="true" style="width: 100%">
+        <el-table-column sortable prop="firstName" label="First Name" width="180"> </el-table-column>
+        <el-table-column sortable prop="lastName" label="Last Name" width="180"> </el-table-column>
+        <el-table-column sortable prop="email" label="E-mail"> </el-table-column>
+        <el-table-column align="right">
+          <template slot-scope="scope">
+            <el-button icon="fa fa-search" size="mini" round type="success" @click="handleView(scope.row)"> View</el-button>
+            <el-button icon="fa fa-paint-brush" size="mini" round @click="handleEdit(scope.row)"> Edit</el-button>
+            <el-button icon="fa fa-trash" size="mini" round type="danger" @click="handleDelete(scope.row)"> Delete</el-button>
+          </template>
+        </el-table-column>
+      </el-table>
+    </article>
     <register ref="registerDialog"></register>
   </section>
 </template>
@@ -40,10 +41,29 @@ export default {
     handleRegister () {
       this.$refs.registerDialog.handleOpen()
     },
-    handleEdit () {
-      this.$refs.registerDialog.handleOpen()
+    handleEdit (user) {
+      this.$refs.registerDialog.handleOpen(user)
     },
-    handleDelete () {}
+    handleView (user) {
+      this.$refs.registerDialog.handleOpenView(user)
+    },
+    handleDelete (user) {
+      this.$setDialogQuestion({
+        title: 'Delete user',
+        message: `Are you sure you want to delete the user "${user.firstName} ${user.lastName}" ?`,
+        callback: () =>
+          this.$remove({
+            urlDispatch: 'User/remove',
+            messages: `User "${user.firstName} ${user.lastName}" successfully deleted`,
+            params: user,
+            callback: () => {
+              this.$list({
+                urlDispatch: 'User/list'
+              })
+            }
+          })
+      })
+    }
   }
 }
 </script>
