25104b2e7d5399c912d867be969432a7ce45b430
Waldener Monteiro <jjrr019@gmail.com>
2020-07-18 03:54:48 -0300

    refactor: adicionando a store de usuários no vuex

--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,9 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
-
+import User from '../pages/users/store'
 Vue.use(Vuex)
-
 export default new Vuex.Store({
-  state: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
   modules: {
+    User
   }
 })
